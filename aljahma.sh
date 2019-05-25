df8b9643c7e333ef9597be9479a73fa423f8e66a
aljahma7341 <32393298+aljahma7341@users.noreply.github.com>
2019-05-26 01:39:57 +0300

    aljahma.sh

--- aljahma.sh
+++ aljahma.sh
@@ -1,1 +1,25 @@
+#!/data/data/com.termux/files/usr/bin/bash
+echo "##############################################"
+
+
+
+# Metasploit version
+msf_ver=5.20.0
+
+echo "WAIT UNTIL INSTALLING............" 
+
+echo "####################################"
+
 apt update && apt upgrade && apt install unstable-repo && apt install metasploit
+
+echo "####################################"
+
+echo "Downloading & Extracting....."
+
+cd $HOME/metasploit-framework
+
+echo "####################################"
+echo "Thanx  To  Hax4us"
+echo "####################################"
+echo " NOW YOU CAN LAUNCH METASPLOIT BY JUST EXECUTE THE COMMAND :=> msfconsole"
+echo "####################################"
