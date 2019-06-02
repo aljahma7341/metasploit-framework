283dbe0175a2f4a7aff4b890055c40f2434acf78
aljahma7341 <32393298+aljahma7341@users.noreply.github.com>
2019-06-03 00:12:07 +0300

    aljahma.sh

--- aljahma.sh
+++ aljahma.sh
@@ -16,7 +16,7 @@ echo "####################################"
 
 apt upgrade
 echo "####################################"
-apt-get install build-essential libreadline-dev libssl-dev libpq5 libpq-dev libreadline5 libsqlite3-dev libpcap-dev git-core autoconf postgresql pgadmin3 curl zlib1g-dev libxml2-dev libxslt1-dev libyaml-dev curl zlib1g-dev gawk bison libffi-dev libgdbm-dev libncurses5-dev libtool sqlite3 libgmp-dev gnupg2 dirmngr
+apt install build-essential libreadline-dev libssl-dev libpq5 libpq-dev libreadline5 libsqlite3-dev libpcap-dev git-core autoconf postgresql pgadmin3 curl zlib1g-dev libxml2-dev libxslt1-dev libyaml-dev curl zlib1g-dev gawk bison libffi-dev libgdbm-dev libncurses5-dev libtool sqlite3 libgmp-dev gnupg2 dirmngr
 echo "####################################"
 apt install metasploit-framework
 
