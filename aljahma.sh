12c8a49872e4f6a30a5650a263b5db9a3b68589e
aljahma7341 <32393298+aljahma7341@users.noreply.github.com>
2019-06-02 22:03:45 +0300

    aljahma.sh

--- aljahma.sh
+++ aljahma.sh
@@ -17,7 +17,10 @@ echo "####################################"
 apt upgrade
 echo "####################################"
 
-apt install unstable-repo
+
+
+sudo apt-get install build-essential libreadline-dev libssl-dev libpq5 libpq-dev libreadline5 libsqlite3-dev libpcap-dev git-core autoconf postgresql pgadmin3 curl zlib1g-dev libxml2-dev libxslt1-dev libyaml-dev curl zlib1g-dev gawk bison libffi-dev libgdbm-dev libncurses5-dev libtool sqlite3 libgmp-dev gnupg2 dirmngr
+
 echo "####################################"
 
 
