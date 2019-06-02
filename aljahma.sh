4a6dd1241480d7ef1acf8246c0c52a8cf62d96b1
aljahma7341 <32393298+aljahma7341@users.noreply.github.com>
2019-06-03 00:15:53 +0300

    aljahma.sh

--- aljahma.sh
+++ aljahma.sh
@@ -9,21 +9,10 @@ msf_ver=5.20.0
 echo "WAIT UNTIL INSTALLING............" 
 
 echo "####################################"
-
-apt update
-
-echo "####################################"
-
-apt upgrade
-echo "####################################"
 apt install build-essential libreadline-dev libssl-dev libpq5 libpq-dev libreadline5 libsqlite3-dev libpcap-dev git-core autoconf postgresql pgadmin3 curl zlib1g-dev libxml2-dev libxslt1-dev libyaml-dev curl zlib1g-dev gawk bison libffi-dev libgdbm-dev libncurses5-dev libtool sqlite3 libgmp-dev gnupg2 dirmngr
 echo "####################################"
 apt install metasploit-framework
-
 echo "####################################"
-
-
-
 apt install metasploit
 echo "####################################"
 echo " NOW YOU CAN LAUNCH METASPLOIT BY JUST EXECUTE THE COMMAND :=> msfconsole"
